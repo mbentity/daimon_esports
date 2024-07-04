b894bb1bc86a0d66d4d50ea793f343b120d64ae0
mbentity <dariobizzarri02@gmail.com>
2024-07-05 01:29:57 +0200

    fixed api calls

--- daimon_esports/daimon_esports_app/serializers.py
+++ daimon_esports/daimon_esports_app/serializers.py
@@ -4,34 +4,34 @@ from .models import User, Discipline, Tournament, Roster, Game, Player, Request
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['__all__']
+        fields = '__all__'
 
 class DisciplineSerializer(serializers.ModelSerializer):
     class Meta:
         model = Discipline
-        fields = ['__all__']
+        fields = '__all__'
 
 class TournamentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tournament
-        fields = ['__all__']
+        fields = '__all__'
 
 class RosterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Roster
-        fields = ['__all__']
+        fields = '__all__'
 
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
-        fields = ['__all__']
+        fields = '__all__'
 
 class PlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
-        fields = ['__all__']
+        fields = '__all__'
 
 class RequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Request
-        fields = ['__all__']
+        fields = '__all__'
