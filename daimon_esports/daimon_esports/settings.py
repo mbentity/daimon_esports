011674328ce3725084941a397771b84961fd6a87
mbentity <dariobizzarri02@gmail.com>
2024-07-06 10:42:48 +0200

    fixed auth cors

--- daimon_esports/daimon_esports/settings.py
+++ daimon_esports/daimon_esports/settings.py
@@ -54,17 +54,19 @@ AUTH_USER_MODEL = 'daimon_esports_app.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'daimon_esports.urls'
 
--- daimon_esports/daimon_esports_app/views.py
+++ daimon_esports/daimon_esports_app/views.py
@@ -1,6 +1,5 @@
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User, Discipline, Tournament, Roster, Game, Player, Request
 from .serializers import RegisterSerializer, LogoutSerializer, UserSerializer, DisciplineSerializer, TournamentSerializer, TournamentSearchSerializer, RosterSerializer, GameSerializer, PlayerSerializer, RequestSerializer
 from django.http import HttpResponse
@@ -12,23 +11,17 @@ from rest_framework import filters
 def index(request):
     return HttpResponse("Daimon Esports API", status=status.HTTP_200_OK)
 
-class RegisterUser(generics.CreateAPIView):
+class UserView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    def get(self, request, *args, **kwargs):
+        return Response(self.serializer_class(request.user).data)
+
+class UserRegister(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
 
-class LoginUser(generics.CreateAPIView):
-    serializer_class = UserSerializer
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        refresh = RefreshToken.for_user(user)
-        return Response({
-            'refresh': str(refresh),
-            'access': str(refresh.access_token),
-        })
-
-class LogoutUser(generics.CreateAPIView):
+class UserLogout(generics.CreateAPIView):
     serializer_class = LogoutSerializer
     permission_classes = [permissions.IsAuthenticated]
     def post(self, request, *args, **kwargs):
@@ -38,11 +31,10 @@ class LogoutUser(generics.CreateAPIView):
             pass
         return Response("Logged out", status=status.HTTP_200_OK)
 
-class Auth(generics.CreateAPIView):
+class UserAuthenticated(generics.CreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     def get(self, request, *args, **kwargs):
         return HttpResponse("Authenticated", status=status.HTTP_200_OK)
-
 
 class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
